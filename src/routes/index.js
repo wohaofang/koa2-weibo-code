f54378b183353375eeeb8f65bfb8e00a21daa334
Dm <dingming@zuoyebang.com>
2020-12-22 15:56:29 +0800

    feat: 添加ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    msg: '111',
+    isMe: true
   })
 })
 
@@ -13,6 +15,14 @@ router.get('/string', async (ctx, next) => {
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page ' + userName,
+    userName
   }
 })
 
